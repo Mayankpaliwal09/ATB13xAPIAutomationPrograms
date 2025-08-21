69a7f4e4a85442b24bd29d67db8cdf4c306dbbbc
Mayank Paliwal <hunnypaliwal09@gmail.com>
2025-08-21 22:58:16 +0530

    get post homework 5th August

---
+++ src/test/java/com/mayankPaliwal/Sample/Homework_5th_august/Get_Post_TestNG.java
@@ -0,0 +1,74 @@
+package com.mayankPaliwal.Sample.Homework_5th_august;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Get_Post_TestNG {
+
+
+
+    // GET ping request
+    @Test
+    public void ping(){
+
+
+        RestAssured.given()
+                .baseUri("https://restful-booker.herokuapp.com")
+                .basePath("/ping")
+                .when()
+                .get()
+                .then().log().all().statusCode(201);
+    }
+
+
+    // Create Booking
+    @Test
+    public void createBooking(){
+
+        Map<String,String> bookingDates = new HashMap<>();
+        bookingDates.put( "checkin","2025-08-25");
+        bookingDates.put( "checkout","2025-08-25");
+
+        Map<String,Object> booking = new HashMap<>();
+        booking.put("firstname","mayank");
+        booking.put("lastname","paliwal");
+        booking.put("totalprice",111);
+        booking.put("depositpaid",true);
+        booking.put("bookingdates",bookingDates);
+        booking.put( "additionalneeds" ,"Breakfast");
+
+        RestAssured.given()
+                .baseUri("https://restful-booker.herokuapp.com")
+                .basePath("/booking")
+                .header("Content-Type","application/json")
+                .body(booking)
+                .when().log().all().post()
+                .then().log().all().statusCode(200);
+    }
+
+    // GET booking
+    @Test
+    public void getBooking(){
+        int bookingId = 2666;
+       String response =  RestAssured.given()
+                .baseUri("https://restful-booker.herokuapp.com")
+                .basePath("/booking/{id}")
+               .pathParam("id", bookingId)
+
+               .accept(ContentType.JSON)
+                .when().log().all().get()
+                .then().log().all().statusCode(418)
+               .extract()
+               .asString();
+
+        System.out.println("response "+response);
+
+
+
+    }
+
+}
